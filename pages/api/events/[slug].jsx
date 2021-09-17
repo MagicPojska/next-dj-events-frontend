b0543b8fe84b476f3900ce3004078b83698b095d
Sam Sepiel <pojskicsafet@gmail.com>
2021-09-17 11:14:41 +0200

    Added evenets on home page and events page

--- pages/api/events/[slug].jsx
+++ pages/api/events/[slug].jsx
@@ -4,7 +4,7 @@ export default function handler(req, res) {
     const evt = events.filter(ev => ev.slug === req.query.slug);
 
     if (req.method === 'GET') {
-        res.status(200).json({ evt })
+        res.status(200).json(evt)
     } else {
         res.setHeader('Allow', ['GET'])
         res.status(405).json({ message: `Method ${req.method} is not allowed` })
