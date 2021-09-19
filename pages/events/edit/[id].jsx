d7f389c33f18237596414ce43cf266199f3f1954
MagicPojska <pojskicsafet@gmail.com>
2021-09-19 16:50:57 +0200

    Added image preview

--- pages/events/edit/[id].jsx
+++ pages/events/edit/[id].jsx
@@ -1,13 +1,13 @@
 import moment from "moment";
+import { FaImage } from "react-icons/fa";
 import { useState } from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
+import Image from "next/image";
 import { API_URL } from "@/config/index";
 import Layout from "@/components/Layout";
-
 import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
-
 import styles from '@/styles/Form.module.css'
 
 export default function EditEventPage({ evt }) {
@@ -20,6 +20,8 @@ export default function EditEventPage({ evt }) {
         time: evt.time,
         description: evt.description,
     })
+
+    const [imagePreview, setImagePreview] = useState(evt.image ? evt.image.formats.thumbnail.url : null);
 
     const router = useRouter();
 
@@ -137,6 +139,19 @@ export default function EditEventPage({ evt }) {
 
                 <input type='submit' value='Update Event' className='btn' />
             </form >
+
+            <h2>Event Image</h2>
+            {imagePreview ? (
+                <Image src={imagePreview} height={100} width={170} />
+            ) :
+                <div>
+                    <p>No image uploaded</p>
+                </div>
+            }
+            <div>
+                <button className='btn-secondary'>
+                    <FaImage /> Set Image</button>
+            </div>
         </Layout >
     );
 }
