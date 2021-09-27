e7d13f2b05bf097489d77a050797f6099455dc4d
MagicPojska <pojskicsafet@gmail.com>
2021-09-27 23:19:22 +0200

    We can add images now because we added the token to post request

--- pages/events/edit/[id].jsx
+++ pages/events/edit/[id].jsx
@@ -170,7 +170,7 @@ export default function EditEventPage({ evt, token }) {
                     <FaImage /> Set Image</button>
             </div>
             <Modal show={showModal} onClose={() => setShowModal(false)}>
-                <ImageUpload evtId={evt.id} imageUploaded={imageUploaded} />
+                <ImageUpload evtId={evt.id} imageUploaded={imageUploaded} token={token} />
             </Modal>
         </Layout >
     );
